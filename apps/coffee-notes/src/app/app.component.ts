70a0dfd986c19932a55e8e02d154a622a9b5c562
ban117 <bcam117@gmail.com>
2023-02-16 10:42:50 +0100

    Initial commit

---
+++ apps/coffee-notes/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bn-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'coffee-notes';
+}
