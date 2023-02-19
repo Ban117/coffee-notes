2585a5f5ac676b97004f027442475b24a88e9f1f
ban117 <bcam117@gmail.com>
2023-02-19 08:51:29 +0100

    feat(deps): add NGXS

--- libs/web/shell/feature/src/lib/web-shell.module.ts
+++ libs/web/shell/feature/src/lib/web-shell.module.ts
@@ -5,6 +5,11 @@ import { LayoutComponent } from "@bn/web/shell/ui/layout";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AngularFireModule } from "@angular/fire/compat";
 import { environment } from "@bn/shared/environments";
+import { NgxsModule } from "@ngxs/store";
+import { NgxsReduxDevtoolsPluginModule } from "@ngxs/devtools-plugin";
+import { NgxsLoggerPluginModule } from "@ngxs/logger-plugin";
+
+export const APP_STATE = [{}];
 
 export const webShellRoutes: Routes = [
 	{
@@ -19,6 +24,9 @@ export const webShellRoutes: Routes = [
 		RouterModule.forRoot(webShellRoutes),
 		BrowserAnimationsModule,
 		AngularFireModule.initializeApp(environment.firebaseConfig),
+		NgxsModule.forRoot([]),
+		NgxsReduxDevtoolsPluginModule.forRoot(),
+		NgxsLoggerPluginModule.forRoot({}),
 	],
 	exports: [RouterModule],
 })
