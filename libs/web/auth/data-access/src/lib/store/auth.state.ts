9c30adc836c1d525cb62f5fe3a2e52de5e921287
ban117 <bcam117@gmail.com>
2023-02-27 12:55:26 +0100

    feat(*): mv redirect to store

--- libs/web/auth/data-access/src/lib/store/auth.state.ts
+++ libs/web/auth/data-access/src/lib/store/auth.state.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/member-ordering */
-import { Injectable } from "@angular/core";
+import { Injectable, NgZone } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 import { Action, Selector, State, StateContext } from "@ngxs/store";
 import firebase from "firebase/compat/app";
 import { take, tap } from "rxjs";
@@ -17,7 +18,12 @@ export interface AuthStateModel {
 })
 @Injectable()
 export class AuthState {
-	constructor(private authService: AuthService) {}
+	constructor(
+		private authService: AuthService,
+		private router: Router,
+		private route: ActivatedRoute,
+		private ngZone: NgZone,
+	) {}
 
 	@Selector()
 	static user(state: AuthStateModel): firebase.User | null {
@@ -42,13 +48,16 @@ export class AuthState {
 		ctx: StateContext<AuthStateModel>,
 		{ email, password }: AuthActions.Login,
 	) {
-		return this.authService
-			.login$(email, password)
-			.pipe(
-				tap(userCredential =>
-					ctx.patchState({ user: userCredential.user }),
+		return this.authService.login$(email, password).pipe(
+			tap(userCredential =>
+				ctx.patchState({ user: userCredential.user }),
+			),
+			tap(() =>
+				this.ngZone.run(() =>
+					this.router.navigate(["/home"], { relativeTo: this.route }),
 				),
-			);
+			),
+		);
 	}
 
 	@Action(AuthActions.Signup)
@@ -56,19 +65,29 @@ export class AuthState {
 		ctx: StateContext<AuthStateModel>,
 		{ email, password }: AuthActions.Signup,
 	) {
-		return this.authService
-			.signUp$(email, password)
-			.pipe(
-				tap(userCredential =>
-					ctx.patchState({ user: userCredential.user }),
+		return this.authService.signUp$(email, password).pipe(
+			tap(userCredential =>
+				ctx.patchState({ user: userCredential.user }),
+			),
+			tap(() =>
+				this.ngZone.run(() =>
+					this.router.navigate(["/home"], { relativeTo: this.route }),
 				),
-			);
+			),
+		);
 	}
 
 	@Action(AuthActions.Logout)
 	logout(ctx: StateContext<AuthStateModel>) {
-		return this.authService.logout$().then(() => {
-			ctx.patchState({ user: null });
-		});
+		return this.authService
+			.logout$()
+			.then(() => {
+				ctx.patchState({ user: null });
+			})
+			.then(() =>
+				this.ngZone.run(() =>
+					this.router.navigate(["/auth"], { relativeTo: this.route }),
+				),
+			);
 	}
 }
--- libs/web/shell/ui/layout/src/lib/layout.component.ts
+++ libs/web/shell/ui/layout/src/lib/layout.component.ts
@@ -2,16 +2,12 @@ import {
 	Component,
 	ViewEncapsulation,
 	ChangeDetectionStrategy,
-	OnInit,
-	OnDestroy,
 } from "@angular/core";
 import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
-import { Observable, Subject } from "rxjs";
-import { map, shareReplay, takeUntil } from "rxjs/operators";
+import { Observable } from "rxjs";
+import { map, shareReplay } from "rxjs/operators";
 import { Store } from "@ngxs/store";
-import { AuthActions, AuthState } from "@bn/web/auth/data-access";
-import { ActivatedRoute, Router } from "@angular/router";
-import { tapWhen } from "@bn/web/shared/utils";
+import { AuthActions } from "@bn/web/auth/data-access";
 
 @Component({
 	selector: "bn-layout",
@@ -21,7 +17,7 @@ import { tapWhen } from "@bn/web/shared/utils";
 	encapsulation: ViewEncapsulation.None,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LayoutComponent implements OnInit, OnDestroy {
+export class LayoutComponent {
 	isHandset$: Observable<boolean> = this.breakpointObserver
 		.observe(Breakpoints.Handset)
 		.pipe(
@@ -29,43 +25,13 @@ export class LayoutComponent implements OnInit, OnDestroy {
 			shareReplay(),
 		);
 
-	user$ = this.store.select(AuthState.user);
-
-	private readonly _destroy$ = new Subject<void>();
-
 	constructor(
 		private breakpointObserver: BreakpointObserver,
 		private store: Store,
-		private router: Router,
-		private route: ActivatedRoute,
 	) {}
-
-	ngOnInit(): void {
-		this.user$
-			.pipe(
-				tapWhen(
-					user => !!user,
-					() =>
-						this.router.navigate(["/home"], {
-							relativeTo: this.route,
-						}),
-					() =>
-						this.router.navigate(["/auth"], {
-							relativeTo: this.route,
-						}),
-				),
-				takeUntil(this._destroy$),
-			)
-			.subscribe();
-	}
 
 	// todo rm, debugging only
 	logout() {
 		this.store.dispatch(new AuthActions.Logout());
-	}
-
-	ngOnDestroy() {
-		this._destroy$.next();
-		this._destroy$.complete();
 	}
 }
