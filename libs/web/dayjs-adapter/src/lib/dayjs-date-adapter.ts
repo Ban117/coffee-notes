3b0508de856a4e7071b51cab66bb82f26df1ad0a
ban117 <bcam117@gmail.com>
2023-03-14 16:13:06 +0100

    fix(dayjs adapter):

--- libs/web/dayjs-adapter/src/lib/dayjs-date-adapter.ts
+++ libs/web/dayjs-adapter/src/lib/dayjs-date-adapter.ts
@@ -12,10 +12,10 @@ import * as customParseFormat from "dayjs/plugin/customParseFormat";
 
 export const MAT_DAYJS_DATE_FORMATS: MatDateFormats = {
 	parse: {
-		dateInput: "MM/DD/YYYY",
+		dateInput: "l",
 	},
 	display: {
-		dateInput: "MM/DD/YYYY",
+		dateInput: "DD/MM/YYYY",
 		monthYearLabel: "MMM YYYY",
 		dateA11yLabel: "LL",
 		monthYearA11yLabel: "MMMM YYYY",
@@ -32,7 +32,6 @@ function range<T>(length: number, valueFunction: (index: number) => T): T[] {
 
 // https://www.npmjs.com/package/@tabuckner/material-dayjs-adapter
 export class DayjsAdapter extends DateAdapter<_dayjs.Dayjs> {
-	// todo `!`
 	private localeData!: {
 		firstDayOfWeek: number;
 		longMonths: string[];
@@ -74,6 +73,126 @@ export class DayjsAdapter extends DateAdapter<_dayjs.Dayjs> {
 		_dayjs.extend(localeData);
 
 		this.setLocale(dateLocale);
+	}
+
+	// https://github.com/2sic/eav-ui
+	parse(value: unknown, parseFormat: string): _dayjs.Dayjs | null {
+		if (value && typeof value === "string") {
+			const longDateFormat = _dayjs()
+				.localeData()
+				.longDateFormat(parseFormat); // MM/DD/YYY or DD-MM-YYYY, etc.
+
+			let parsed = _dayjs(value, longDateFormat, this.locale);
+
+			if (parsed.isValid()) {
+				// string value is exactly like long date format
+				return parsed;
+			}
+
+			if (value.length === 9) {
+				// user might have typed 1-12-2020 or 12/1/2020
+				// try to parse with D-MM-YYYY or MM/D/YYYY (based on long date format)
+				const formatWithSmallDay = longDateFormat.replace("DD", "D");
+				parsed = _dayjs(value, formatWithSmallDay, this.locale);
+				if (parsed.isValid()) {
+					return parsed;
+				}
+
+				// user might have typed 25-1-2020 or 1/25/2020
+				// try to parse with DD-M-YYYY or M/DD/YYYY (based on long date format)
+				const formatWithSmallMonth = longDateFormat.replace("MM", "M");
+				parsed = _dayjs(value, formatWithSmallMonth, this.locale);
+				if (parsed.isValid()) {
+					return parsed;
+				}
+			}
+
+			if (value.length === 8) {
+				// user might have typed 24012020 or 01242020
+				// strip long date format of non-alphabetic characters so we get MMDDYYYY or DDMMYYYY
+				const formatWithoutSeparators = longDateFormat.replace(
+					/[\W_]+/g,
+					"",
+				);
+				parsed = _dayjs(value, formatWithoutSeparators, this.locale);
+				if (parsed.isValid()) {
+					return parsed;
+				}
+
+				// user might have typed 1-2-2020 or 2/1/2020
+				// try to parse with D-M-YYYY or M/D/YYYY (based on long date format)
+				const formatWithSmallDayAndMonth = longDateFormat
+					.replace("DD", "D")
+					.replace("MM", "M");
+				parsed = _dayjs(value, formatWithSmallDayAndMonth, this.locale);
+				if (parsed.isValid()) {
+					return parsed;
+				}
+			}
+
+			if (value.length < 6 && value.length > 2) {
+				// user might have typed 01/24, 24-01, 1/24, 24/1 or 24-1
+				// try to extract month and day part and parse them with custom format
+				let parts: string[] = [];
+				if (value.indexOf("/") !== -1) {
+					parts = value.split("/");
+				}
+				if (value.indexOf("-") !== -1) {
+					parts = value.split("-");
+				}
+				if (value.indexOf(".") !== -1) {
+					parts = value.split(".");
+				}
+				if (parts.length === 2) {
+					let dayPart = "";
+					let monthPart = "";
+
+					if (longDateFormat.startsWith("D")) {
+						dayPart = parts[0];
+						monthPart = parts[1];
+					} else if (parts.length > 1) {
+						monthPart = parts[0];
+						dayPart = parts[1];
+					}
+					if (monthPart.length === 1) {
+						monthPart = 0 + monthPart;
+					}
+					if (dayPart.length === 1) {
+						dayPart = 0 + dayPart;
+					}
+					parsed = _dayjs(dayPart + monthPart, "DDMM", this.locale);
+					if (parsed.isValid()) {
+						return parsed;
+					}
+				}
+			}
+
+			if (value.length === 2) {
+				// user might have typed 01, parse DD only
+				const format = "DD";
+				parsed = _dayjs(value, format, this.locale);
+				if (parsed.isValid()) {
+					return parsed;
+				}
+			}
+
+			if (value.length === 1) {
+				// user might have typed 1, parse D only
+				const format = "D";
+				parsed = _dayjs(value, format, this.locale);
+
+				if (parsed.isValid()) {
+					return parsed;
+				}
+			}
+
+			// not able to parse anything sensible, return something invalid so input can be corrected
+			return _dayjs(null);
+		}
+
+		return value && value instanceof _dayjs.Dayjs
+			? _dayjs(value).locale(this.locale)
+			: null;
 	}
 
 	getYear(date: _dayjs.Dayjs): number {
@@ -130,19 +249,6 @@ export class DayjsAdapter extends DateAdapter<_dayjs.Dayjs> {
 
 	today(): _dayjs.Dayjs {
 		return _dayjs();
-	}
-	parse(value: unknown, parseFormat: string): _dayjs.Dayjs | null {
-		if (value && typeof value === "string") {
-			return _dayjs(
-				value,
-				_dayjs().localeData().longDateFormat(parseFormat),
-				this.locale,
-			);
-		}
-
-		return value && value instanceof _dayjs.Dayjs
-			? _dayjs(value).locale(this.locale)
-			: null;
 	}
 
 	format(date: _dayjs.Dayjs, displayFormat: string): string {
