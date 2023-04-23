8eb1f3ea9d90c3c87023d6f5b0a179a4aa990cdc
ban117 <bcam117@gmail.com>
2023-04-23 15:26:03 +0200

    refactor(gradient): simplify

--- libs/web/shared/directives/gradient/src/lib/gradient.directive.ts
+++ libs/web/shared/directives/gradient/src/lib/gradient.directive.ts
@@ -10,7 +10,6 @@ import {
 	PLATFORM_ID,
 	Renderer2,
 } from "@angular/core";
-import { take, tap } from "rxjs";
 
 @Directive({
 	selector: "[bnGradient]",
@@ -22,7 +21,7 @@ export class GradientDirective implements AfterViewInit {
 
 	@Input() bnGradientMode: "lighten" | "darken" = "darken";
 
-	isBrowser: boolean;
+	private isBrowser: boolean;
 
 	constructor(
 		@Optional() @Inject(PLATFORM_ID) platformId: string,
@@ -40,46 +39,33 @@ export class GradientDirective implements AfterViewInit {
 			return;
 		}
 
-		this._ngZone.onStable
-			.pipe(
-				take(1),
-				tap(() => {
-					const currentBgColor = this.window.getComputedStyle(
-						this.el.nativeElement,
-					).backgroundColor;
+		const currentBgColor = this.window.getComputedStyle(
+			this.el.nativeElement,
+		).backgroundColor;
 
-					const newBgColor = this.gradateColor(
-						currentBgColor,
-						this.gradientLevel,
-					);
+		const newBgColor = this.gradateColor(
+			currentBgColor,
+			this.gradientLevel,
+		);
 
-					this.renderer.setStyle(
-						this.el.nativeElement,
-						"backgroundColor",
-						newBgColor,
-					);
-				}),
-			)
-			.subscribe();
+		this.renderer.setStyle(
+			this.el.nativeElement,
+			"backgroundColor",
+			newBgColor,
+		);
 	}
 
 	private gradateColor(color: string, level: number): string {
-		const gradatedValue = level * this.bnGradientFactor;
+		const gradatedValue =
+			level *
+			this.bnGradientFactor *
+			(this.bnGradientMode === "darken" ? -1 : 1);
+
 		const rgb = color
 			.replace(/^(rgb|rgba)\(/, "")
 			.replace(/\)$/, "")
 			.split(",")
 			.map(parseFloat);
-
-		if (this.bnGradientMode === "darken") {
-			const newRgb = {
-				r: Math.max(0, rgb[0] - gradatedValue),
-				g: Math.max(0, rgb[1] - gradatedValue),
-				b: Math.max(0, rgb[2] - gradatedValue),
-			};
-
-			return `rgb(${newRgb.r}, ${newRgb.g}, ${newRgb.b})`;
-		}
 
 		const newRgb = {
 			r: Math.max(0, rgb[0] + gradatedValue),
